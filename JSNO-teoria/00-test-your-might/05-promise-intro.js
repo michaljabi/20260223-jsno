c3eb588a651b0812288cea1ee2e19ca2e0a8e2e7
Jabi <michal.jabi@gmail.com>
2026-02-23 14:53:11 +0100

    explain(theory-05): make it work - handle Promise

--- JSNO-teoria/00-test-your-might/05-promise-intro.js
+++ JSNO-teoria/00-test-your-might/05-promise-intro.js
@@ -1,12 +1,33 @@
 
 console.log('Hello stranger... shall we start?')
 
+// PROVIDER
 function giveMeTheNumber() {
     return Promise.resolve(300)
 }
+
+// PROVIDER #2 (new API)
+// async function giveMeTheNumberAsync() {
+//     return 300
+// }
 
 /**
  * 1
  *
  * Poniżej odbierz liczbę 300 i pokaż ją na ekranie:
  * */
+
+// CONSUMER:
+// console.log(giveMeTheNumber());
+
+// 1. Classic
+giveMeTheNumber()
+    .then((val) => {
+        // await giveMeTheNumber()
+        console.log(val)
+    })
+
+// 2. async/await syntax (lukier składniowy do odbioru Promise)
+// Inline API (sync)
+const val = await giveMeTheNumber();
+console.log(val)
