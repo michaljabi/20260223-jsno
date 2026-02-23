e013a92aafee18c98ea4dfcbae8bda081882f4bf
Jabi <michal.jabi@gmail.com>
2026-02-23 14:19:19 +0100

    feat(theory-00): make it work. exmplae with setTimeout.

--- JSNO-teoria/00-test-your-might/00-callback-intro.js
+++ JSNO-teoria/00-test-your-might/00-callback-intro.js
@@ -1,8 +1,24 @@
+// @ts-check
 
-console.log('Hello stranger... shall we start?')
+console.log(`Hello 
+    stranger... 
+    shall 
+we
+     start?`);
 
+console.log(2 + 2 * 2);
+
+// function giveMeTheNumber(mistery = (num = 0) => {}) {
+/**
+ *
+ * @param {Function} mistery
+ */
+// PROVIDER
 function giveMeTheNumber(mistery) {
-    mistery(300)
+  mistery(200);
+  setTimeout(() => {
+    mistery(100);
+  }, 5000);
 }
 
 /**
@@ -10,4 +26,20 @@ function giveMeTheNumber(mistery) {
  *
  * Poniżej odbierz liczbę 300 i pokaż ją na ekranie:
  * */
+// CONSUMER:
+giveMeTheNumber((val = 0) => {
+  console.log(val);
+});
 
+// CONSUMER 2:
+giveMeTheNumber((val = 0) => {
+  console.log(val);
+});
+
+// try {
+//     giveMeTheNumber();
+// } catch (err) {
+//     console.log(err.message)
+// }
+
+// console.log('@')
