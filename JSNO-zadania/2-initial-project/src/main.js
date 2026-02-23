01f33ac1d2f7a99ec7ace8e880d6331b44ff1cc3
Jabi <michal.jabi@gmail.com>
2026-02-23 10:30:58 +0100

    task(2): part1 - make it work.

---
+++ JSNO-zadania/2-initial-project/src/main.js
@@ -0,0 +1,14 @@
+console.log(`
+****************************************
+*   WITAJ W GENERATORZE BANNERÓW [!]   *
+****************************************
+`)
+
+// python """
+
+// console.log("Hello
+//     World!")
+
+setInterval(() => {
+    console.log('!')
+}, 2000)
