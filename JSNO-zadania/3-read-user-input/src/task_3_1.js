95943ffe7622b3f22002d3646f79e39adda721e8
Jabi <michal.jabi@gmail.com>
2026-02-23 15:58:43 +0100

    refactor(task-3.1): make it right - async/await + use `node:readline/promises`

--- JSNO-zadania/3-read-user-input/src/task_3_1.js
+++ JSNO-zadania/3-read-user-input/src/task_3_1.js
@@ -1,10 +1,24 @@
 // Popraw i użyj async/await + Promises w `readline`.
-import { createInterface } from "node:readline";
+import { createInterface } from "node:readline/promises";
 import { stdin, stdout, exit } from "node:process";
 
 const rl = createInterface({ input: stdin, output: stdout });
 
 const user = { name: "", lastName: "", age: 0 };
+
+try {
+  user.name = await rl.question("Podaj imie: ");
+  user.lastName = await rl.question("Podaj nazwisko: ");
+  user.age = Number(await rl.question("Podaj wiek: ")) || 0;
+  console.log("Pobrane dane", user);
+} catch (e) {
+  console.error('\nBłąd:', e.message);
+} finally {
+  rl.close();
+  exit(0);
+}
+
+/*
 rl.question("Podaj imie: ", (a) => {
   user.name = a;
   rl.question("Podaj nazwisko: ", (a) => {
@@ -19,3 +33,4 @@ rl.question("Podaj imie: ", (a) => {
     });
   });
 });
+*/
