f4f900a206c5d7a7ef1768e419269437f9adadcc
Jabi <michal.jabi@gmail.com>
2026-02-23 11:12:52 +0100

    task(2.1): explain CommonJS legacy (2009 invented).

---
+++ JSNO-zadania/2-initial-project/src/constants.js
@@ -0,0 +1,10 @@
+const LR_PADDING = 8;
+
+
+// CommonJS export:
+exports.LR_PADDING = LR_PADDING;
+
+exports.otherStuff = 'other stuff';
+
+// Żeby korzystać z CommonJS
+// potrzebujemy JAWNIE wyexportować coś i jawenie zaimportować po 2 stronie.
--- JSNO-zadania/2-initial-project/src/main.js
+++ JSNO-zadania/2-initial-project/src/main.js
@@ -1,5 +1,17 @@
 
-const LR_PADDING = 8;
+// Zadanie 2.1
+
+// Wyodrębnij do innego pliku LR_PADDING
+// I używając składni require() [CommonJS] zaimportuj tutaj.
+// Samodzielnie nazwij plik z LR_PADDING.
+
+// CommonJS import
+// const constants = require("./constants");
+// console.log(constants.LR_PADDING);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring
+const { LR_PADDING } = require("./constants");
+
 const text = "Witaj w generatorze bannerów [!]"
 const border = "*".repeat(text.length + LR_PADDING);
 console.log(`
