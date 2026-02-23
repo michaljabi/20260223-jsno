5a0452afbc5604c419f8138b7b8fd35d9e69b99b
Jabi <michal.jabi@gmail.com>
2026-02-23 11:16:04 +0100

    refactor(2.1): module.exports

--- JSNO-zadania/2-initial-project/src/constants.js
+++ JSNO-zadania/2-initial-project/src/constants.js
@@ -1,10 +1,17 @@
 const LR_PADDING = 8;
 
 
-// CommonJS export:
-exports.LR_PADDING = LR_PADDING;
+// CommonJS export (shortcut):
+//exports.LR_PADDING = LR_PADDING;
 
-exports.otherStuff = 'other stuff';
+//exports.otherStuff = 'other stuff';
 
 // Żeby korzystać z CommonJS
 // potrzebujemy JAWNIE wyexportować coś i jawenie zaimportować po 2 stronie.
+
+
+// CommonJS export pełny wariant:
+module.exports = {
+    LR_PADDING: 8,
+    otherStuff: 'other stuff'
+}
