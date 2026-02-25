122c4123ef4af2ff8b205e1fdc74de7dcdb02a28
Jabi <michal.jabi@gmail.com>
2026-02-25 16:08:35 +0100

    explain(typescript, vite): front-end + tsc

--- JSNO-przyklady/back-end/node-with-typescript/src/main.ts
+++ JSNO-przyklady/back-end/node-with-typescript/src/main.ts
@@ -1,9 +1,9 @@
-import { Car } from './Car'
+import { Car } from "./Car";
 
-console.log('Hello in my program')
+console.log("Hello in my program");
 
 const myCar = new Car();
-const myOtherCar = new Car('Audi', 'A8');
+const myOtherCar = new Car("Audi?", "A8");
 
 myCar.sayNameAndModel();
 myOtherCar.sayNameAndModel();
--- JSNO-przyklady/front-end/bundler-vite-example/src/main.js
+++ JSNO-przyklady/front-end/bundler-vite-example/src/main.js
@@ -2,6 +2,6 @@ import './style.css'
 import { H2Sample } from './components/h2-sample';
 
 document.querySelector('#app').append(
-	H2Sample({title: 'Dodatkowy nagłówek #2 poziomu...'}),
+	H2Sample({title: 'Dodatkowy nagłówek #2 poziomu...Xd'}),
 	H2Sample({title: '... i kolejny nagłówek #2 ', sign: '🍄'}),
 );
